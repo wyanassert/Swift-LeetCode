4463d4f74609c95ee50d9ceb4f3719a4279a423e
wyanassert <1402099772@qq.com>
2020-07-23 10:53:18 +0800

    add 116 non-recursive solution for Populating Next Right Pointers in Each Node

--- Swift-LeetCode/116_Populating_Next_Right_Pointers_in_Each_Node.swift
+++ Swift-LeetCode/116_Populating_Next_Right_Pointers_in_Each_Node.swift
@@ -9,24 +9,50 @@
 import Cocoa
 
 class _116_Populating_Next_Right_Pointers_in_Each_Node: NSObject {
-    func traversal(_ root: Node?) {
-        guard let root = root else { return }
-        
-        guard let left = root.left, let right = root.right else { return }
-        
-        left.next = right
-        if let next = root.next {
-            right.next = next.left
-        }
-        traversal(root.left)
-        traversal(root.right)
-    }
-    
     func connect(_ root: Node?) -> Node? {
         guard let root = root else { return nil }
         
-        traversal(root)
-        
+        var tmp: Node? = root
+        var first: Node?
+        while tmp != nil {
+            if first == nil {
+                first = tmp?.left //prepare to next level
+            }
+            if let left = tmp?.left {
+                left.next = tmp?.right
+            } else {
+                break //end
+            }
+            if let next = tmp?.next, let right = tmp?.right {
+                right.next = next.left
+                tmp = next
+                continue //to next point in the same level
+            } else {
+                tmp = first //go to next level
+                first = nil
+            }
+        }
         return root
     }
+    //递归解法
+//    func traversal(_ root: Node?) {
+//        guard let root = root else { return }
+//
+//        guard let left = root.left, let right = root.right else { return }
+//
+//        left.next = right
+//        if let next = root.next {
+//            right.next = next.left
+//        }
+//        traversal(root.left)
+//        traversal(root.right)
+//    }
+//
+//    func connect(_ root: Node?) -> Node? {
+//        guard let root = root else { return nil }
+//
+//        traversal(root)
+//
+//        return root
+//    }
 }
---
+++ Swift-LeetCode/117_Populating_Next_Right_Pointers_in_Each_Node_2.swift
@@ -0,0 +1,15 @@
+//
+//  117_Populating_Next_Right_Pointers_in_Each_Node_2.swift
+//  Swift-LeetCode
+//
+//  Created by wyan on 2020/7/23.
+//  Copyright © 2020 wyanassert. All rights reserved.
+//
+
+import Cocoa
+
+class _117_Populating_Next_Right_Pointers_in_Each_Node_2: NSObject {
+    func connect(_ root: Node?) -> Node? {
+        return nil
+    }
+}
