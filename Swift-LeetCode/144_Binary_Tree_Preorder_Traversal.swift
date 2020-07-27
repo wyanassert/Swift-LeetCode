f87b42ccea0af5359ace151afb83dfc8c174a952
wyanassert <1402099772@qq.com>
2020-07-27 14:32:16 +0800

    non-recursive for 144

--- Swift-LeetCode/144_Binary_Tree_Preorder_Traversal.swift
+++ Swift-LeetCode/144_Binary_Tree_Preorder_Traversal.swift
@@ -9,18 +9,40 @@
 import Cocoa
 
 class _144_Binary_Tree_Preorder_Traversal: NSObject {
-    func traversal(_ root: TreeNode?, _ results: inout [Int]) {
-        guard let root = root else { return }
+    func preorderTraversal(_ root: TreeNode?) -> [Int] {
+        guard let root = root else {
+            return []
+        }
         
-        results.append(root.val)
-        traversal(root.left, &results)
-        traversal(root.right, &results)
+        var result: [Int] = []
+        var stack: [TreeNode] = []
+        stack.append(root)
+        
+        while !stack.isEmpty {
+            let last = stack.removeLast()
+            result.append(last.val)
+            if let right = last.right {
+                stack.append(right)
+            }
+            if let left = last.left {
+                stack.append(left)
+            }
+        }
+        return result
     }
     
-    func preorderTraversal(_ root: TreeNode?) -> [Int] {
-        guard let root = root else { return [] }
-        var results: [Int] = []
-        traversal(root, &results)
-        return results
-    }
+//    func traversal(_ root: TreeNode?, _ results: inout [Int]) {
+//        guard let root = root else { return }
+//
+//        results.append(root.val)
+//        traversal(root.left, &results)
+//        traversal(root.right, &results)
+//    }
+//
+//    func preorderTraversal(_ root: TreeNode?) -> [Int] {
+//        guard let root = root else { return [] }
+//        var results: [Int] = []
+//        traversal(root, &results)
+//        return results
+//    }
 }
