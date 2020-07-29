314f804c8c054da266d3d04d2620fdbd20327e0f
wyanassert <1402099772@qq.com>
2020-07-29 15:02:01 +0800

    simplify 045

--- Swift-LeetCode/045_Jump_Game_2.swift
+++ Swift-LeetCode/045_Jump_Game_2.swift
@@ -10,24 +10,39 @@ import Cocoa
 
 class _045_Jump_Game_2: NSObject {
     func jump(_ nums: [Int]) -> Int {
-        var step = 0
-        var index = 0
-        var currMax = 0
-        var nextMax = 0
+        var level = 0
+        var nextStep = 0
+        var currMaxPosi = 0
         
-        while index < nums.count {
-            if currMax >= nums.count - 1 {
-                return step
+        for i in 0..<nums.count-1
+        {
+            nextStep = max(i + nums[i], nextStep)
+            if (i == currMaxPosi) {
+                level += 1
+                currMaxPosi = nextStep
             }
-            while index <= currMax {
-                nextMax = max(nextMax, index + nums[index])
-                index += 1
-            }
-            currMax = nextMax
-            step += 1
         }
-        return step
+        return level
     }
+//    func jump(_ nums: [Int]) -> Int {
+//        var step = 0
+//        var index = 0
+//        var currMax = 0
+//        var nextMax = 0
+//
+//        while index < nums.count {
+//            if currMax >= nums.count - 1 {
+//                return step
+//            }
+//            while index <= currMax {
+//                nextMax = max(nextMax, index + nums[index])
+//                index += 1
+//            }
+//            currMax = nextMax
+//            step += 1
+//        }
+//        return step
+//    }
     
 //    LeetCode 看到的答案 思路不是很容易理解
 //    func jump(_ nums: [Int]) -> Int {
