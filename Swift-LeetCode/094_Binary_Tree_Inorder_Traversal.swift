9c957ea385bad4643ce8f81b4be12e51e1f81fa3
wyanassert <1402099772@qq.com>
2020-07-31 16:22:08 +0800

    add non-recursive solution for 094

--- Swift-LeetCode/094_Binary_Tree_Inorder_Traversal.swift
+++ Swift-LeetCode/094_Binary_Tree_Inorder_Traversal.swift
@@ -9,17 +9,43 @@
 import Cocoa
 
 class _094_Binary_Tree_Inorder_Traversal: NSObject {
-    func traversal(_ root: TreeNode?, _ results: inout [Int]) {
-        guard let root = root else { return }
-        
-        traversal(root.left, &results)
-        results.append(root.val)
-        traversal(root.right, &results)
-    }
     func inorderTraversal(_ root: TreeNode?) -> [Int] {
         guard let root = root else { return [] }
+        
         var results: [Int] = []
-        traversal(root, &results)
+        
+        var stack: [(TreeNode, Bool)] = []
+        stack.append((root, false))
+        
+        while !stack.isEmpty {
+            let (node, visit) = stack.removeLast()
+            if visit || (node.left == nil && node.right == nil) {
+                results.append(node.val)
+                continue
+            }
+            if let right = node.right {
+                stack.append((right, false))
+            }
+            stack.append((node, true))
+            if let left = node.left {
+                stack.append((left, false))
+            }
+        }
+        
         return results
     }
+    
+//    func traversal(_ root: TreeNode?, _ results: inout [Int]) {
+//        guard let root = root else { return }
+//
+//        traversal(root.left, &results)
+//        results.append(root.val)
+//        traversal(root.right, &results)
+//    }
+//    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+//        guard let root = root else { return [] }
+//        var results: [Int] = []
+//        traversal(root, &results)
+//        return results
+//    }
 }
