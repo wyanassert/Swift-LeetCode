7f537e32e870a59ee63ec13e60c132b3661cadda
wyanassert <1402099772@qq.com>
2020-07-31 20:35:28 +0800

    add 958

---
+++ Swift-LeetCode/958_Check_Completeness_of_a_Binary_Tree.swift
@@ -0,0 +1,54 @@
+//
+//  958_Check_Completeness_of_a_Binary_Tree.swift
+//  Swift-LeetCode
+//
+//  Created by wyan on 2020/7/31.
+//  Copyright © 2020 wyanassert. All rights reserved.
+//
+
+import Cocoa
+
+class _958_Check_Completeness_of_a_Binary_Tree: NSObject {
+    func isCompleteTree(_ root: TreeNode?) -> Bool {
+        var queue: [TreeNode?] = []
+        queue.append(root)
+        while let curr = queue.popLast() , curr != nil {
+            queue.insert(curr?.left, at: 0)
+            queue.insert(curr?.right, at: 0)
+        }
+        return queue.filter { $0 != nil }.count == 0
+    }
+//    func isCompleteTree(_ root: TreeNode?) -> Bool {
+//        guard let root = root else {
+//            return false
+//        }
+//        var nullBegin = false
+//
+//        var queue: [TreeNode] = []
+//        queue.append(root)
+//
+//        while !queue.isEmpty {
+//            let node = queue.removeFirst()
+//            if node.left == nil {
+//                nullBegin = true
+//            } else {
+//                if nullBegin {
+//                    return false
+//                } else {
+//                    queue.append(node.left!)
+//                }
+//            }
+//            if node.right == nil {
+//                nullBegin = true
+//            } else {
+//                if nullBegin {
+//                    return false
+//                } else {
+//                    queue.append(node.right!)
+//                }
+//            }
+//        }
+//
+//        return true
+//    }
+}
