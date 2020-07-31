16503319640ccaaaa1014c0b8e1e2372fda0a4df
wyanassert <1402099772@qq.com>
2020-07-31 20:08:12 +0800

    add 151

--- Swift-LeetCode/151_Reverse_Words_in_a_String.swift
+++ Swift-LeetCode/151_Reverse_Words_in_a_String.swift
@@ -9,43 +9,37 @@
 import Cocoa
 
 class _151_Reverse_Words_in_a_String: NSObject {
-//    func reverseWords(_ s: String) -> String {
-//        var tmpStr = ""
-//
-//        var stack: [String] = []
-//
-//        for c in Array(s) {
-//            switch c {
-//            case " ":
-//                if tmpStr.count > 0 {
-//                    stack.append(tmpStr)
-//                    tmpStr = ""
-//                }
-//                break
-//            default:
-//                tmpStr += String(c)
-//                break
-//            }
-//        }
-//
-//        if tmpStr.count > 0 {
-//            stack.append(tmpStr)
-//            tmpStr = ""
-//        }
-//
-//        for i in 0..<stack.count {
-//            if i == 0 {
-//                tmpStr = stack[i]
-//            } else {
-//                tmpStr = stack[i] + " " + tmpStr
-//            }
-//        }
-//
-//        return tmpStr
-//    }
     func reverseWords(_ s: String) -> String {
-        var wordList = s.split(separator: " ")
-        wordList.reverse()
-        return wordList.joined(separator: " ")
+        var tmpStr = ""
+
+        var stack: [String] = []
+
+        for c in Array(s) {
+            switch c {
+            case " ":
+                if tmpStr.count > 0 {
+                    stack.append(tmpStr)
+                    tmpStr = ""
+                }
+                break
+            default:
+                tmpStr += String(c)
+                break
+            }
+        }
+
+        if tmpStr.count > 0 {
+            stack.append(tmpStr)
+            tmpStr = ""
+        }
+
+        stack.reverse()
+
+        return stack.joined(separator: " ")
     }
+//    func reverseWords(_ s: String) -> String {
+//        var wordList = s.split(separator: " ")
+//        wordList.reverse()
+//        return wordList.joined(separator: " ")
+//    }
 }
